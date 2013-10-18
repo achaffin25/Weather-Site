e2230f3b6ed677ff46fc9c6cc989fc9953157947
Ashley Chaffin <achaffin25@gmail.com>
2013-10-18 16:34:43 -0700

    input clear

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
+    
+    // Click Button
+    $('button').click(function(){
+    
+        // Store Input
+        var zip = $('input').val();
+        console.log(zip);
 
-	//Click Button
-	$('button').click(function(){
-	
-		//Store Input
-		var zip = $('input').val();	
-		console.log(zip);
-		
-	
-	}); // end click
-	
-	
-	
-	
-
-
-
-
+        // GET WEATHER
+        $.simpleWeather({
+            zipcode: zip,
+            success: function(weather){
+                html = '<h1>' + weather.city + '</h1>';
+                html += '<h3>' + weather.temp + '</h3>';
+                html += '<p>Tomorrow\'s Forecast High: ' + weather.tomorrow.high + '</p>';
+ 
+                
+                // Output on Page
+                $('#weather').html(html);
+            },
+            error: function(error) {
+                $("#weather").html('<p>'+error+'</p>');
+            }
+        });// end weather
+        
+    });//end click
+    
+    // Clear Input on Click
+    $('input:text').click(function(){
+        $('input').val('');
+    });// end clear input    
+    
+});// end document
 }); // end document
