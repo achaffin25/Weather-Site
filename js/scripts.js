2f5c3257e74a61f573db975ae487af8a076a8400
Ashley Chaffin <achaffin25@gmail.com>
2013-10-25 18:33:17 -0700

    Tomorrow's forecast and responsiveness
    * Added a section for tomorrow's forecast
    * Made it responsive

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,14 @@ $(document).ready(function(){
             unit: 'f',
             success: function(weather){
             	
+            	$('body').removeClass();
             	
-            	
-            	html = '<img src="img/icons/' + weather.code + '.svg">';
+            	html = '<img class="todayicon" src="img/icons/' + weather.code + '.svg">';
             	html += '<h1>' + weather.temp + '&deg;' + '<span>' + weather.units.temp + '</span>' + '</h1>';
                 html += '<h3>' + weather.city + ', ' + weather.region + '</h3>';
-                html += '<p>Tomorrow\'s Forecast High: ' + weather.tomorrow.high + '</p>';
-                
+                html += '<p>' + weather.forecast + ' &mdash;  &nbsp;' + weather.high + '&deg;' + ' | ' + weather.low + '&deg;' + '</p>';
+                html += '<hr>';
+                html += '<div class="tomorrow"><h2>Tomorrow\'s Forecast: &nbsp;</h2><img class="tomorrowicon" src="img/icons/' + weather.tomorrow.code + '.svg"><p><span>' + weather.tomorrow.high + '&deg;' + ' | ' + weather.tomorrow.low + '&deg;' + '</span><br>' + weather.tomorrow.forecast + '</p></div>';
                 
                 $('body').addClass('bg' + weather.code);
  
